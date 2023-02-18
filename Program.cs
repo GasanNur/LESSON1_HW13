8ccfc5aa34651a36d43b2a88f77255eb7eb3e5cf
Gasan <1793959@mail.rum>
2023-02-18 21:18:05 +0300

    new commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+int number = ReadInt("Введите число  "); 
+
+if (number < 100 )   
+{
+    Console.WriteLine("третьей цыфры нет ");
+}
+else
+{
+    Console.WriteLine(InCenter(number)); 
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+ int InCenter(int a)
+{
+    while (a>=1000)
+    {
+        a= a / 10;
+    }   
+    a = a % 10;
+    return a;
+}
+
